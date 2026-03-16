9892385df2bbd062910c3777a1047c0184db20db
rian.sousa <rian.sousa@technosoftware.com.br>
2026-03-16 09:30:29 -0300

    criado classe css container

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import {Heading} from "./components/Heading.tsx";
-
 import './styles/theme.css'
 import './styles/global.css'
-import {TimerIcon} from "lucide-react";
 
 export function App() {
-    return (
-        <>
-            <Heading>
-                Olá pessoas!
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>Bilionário se aproxima de um marco inédito ao atingir patrimônio estimado em US$ 800 bilhões,
-                impulsionado pela valorização de seus negócios em tecnologia, especialmente em inteligência
-                artificial.
-            </p>
-        </>
-    );
+    return <>
+        <div className="container">
+            <div className="content">
+                <section>
+                    LOGO
+                </section>
+            </div>
+        </div><div className="container">
+            <div className="content">
+                <section>
+                    MENU
+                </section>
+            </div>
+        </div><div className="container">
+            <div className="content">
+                <section>
+                    FORM
+                </section>
+            </div>
+        </div><div className="container">
+            <div className="content">
+                <section>
+                    FOOTER
+                </section>
+            </div>
+        </div>
+    </>;
 }
