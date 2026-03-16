86f2e35cb69c35034b65a1d1cd8f3790ed582df5
rian.sousa <rian.sousa@technosoftware.com.br>
2026-03-16 12:15:46 -0300

    Modificado nomemclatura das pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/theme.css'
 import './styles/global.css'
-import {Heading} from "./components/Heading";
-import {Container} from "./components/Container";
+import {Heading} from "./components/Heading/Heading.tsx";
+import {Container} from "./components/Container/Container.tsx";
 
 export function App() {
     return <>
