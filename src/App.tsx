f8e0a09202f42fafda4e8e413023d573354bb440
rian.sousa <rian.sousa@technosoftware.com.br>
2026-03-17 13:43:52 -0300

    criado componente de input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {Container} from "./components/Container/Container.tsx";
 import {Logo} from "./components/Logo/Logo.tsx";
 import {Menu} from "./components/Menu/Menu.tsx";
+import {DefaultInput} from "./components/DefaultInput/DefaultInput.tsx";
+import {CountDown} from "./components/CountDown/CountDown.tsx";
 
 import './styles/theme.css'
 import './styles/global.css'
@@ -13,6 +15,36 @@ export function App() {
 
         <Container>
             <Menu/>
+        </Container>
+
+        <Container>
+            <CountDown/>
+        </Container>
+
+        <Container>
+            <form className="form" action="">
+                <div className="formRow">
+                    <DefaultInput
+                        id='meuInput'
+                        type='text'
+                        labelText='asdf'
+                        placeholder='Digite algo'
+                    />
+                </div>
+
+                <div className="formRow">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+
+                <div className="formRow">
+                    <p>Ciclos</p>
+                    <p>0 0 0 0 0 0</p>
+                </div>
+
+                <div className="formRow">
+                    <button>Enviar</button>
+                </div>
+            </form>
         </Container>
     </>;
 }
---
+++ src/components/DefaultInput/DefaultInput.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import style from './DefaultInput.module.css'
+
+type DefaultInputProps = {
+    id: string;
+    labelText: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaultInput({id, type, labelText, ...rest}: DefaultInputProps) {
+    return (
+        <>
+            <label htmlFor={id}>{labelText}</label>
+            <input className={style.input} id={id} type={type} {...rest} />
+        </>
+    )
+}
